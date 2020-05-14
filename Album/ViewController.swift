82df52ecffd15b279e24b166384ac2beaa10cc3a
Chris Price <christopher.r.price@gmail.com>
2020-05-14 17:53:01 -0400

    Reorganized folders.

---
+++ Album/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Album
+//
+//  Created by Chris Price on 5/14/20.
+//  Copyright © 2020 com.chrispriiice. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
